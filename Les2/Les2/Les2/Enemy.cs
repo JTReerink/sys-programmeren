dee22eea0a9b7ef8d062da77726395cf9fe8f1eb
Jaap <jareerink@gmail.com>
2021-11-03 12:42:37 +0100

    first commit

---
+++ Les2/Les2/Les2/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les2
+{
+    class Enemy
+    {
+        public int health = 0;
+        public void Damage(int amount)
+        {
+            health -= amount;
+        }
+    }
+}
---
+++ Les2/Les2/Les2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Les2
+{
+    class Program
+    {
+        //Enemy enemy = new Enemy();
+        static void Main(string[] args)
+        {
+            Enemy enemy = new Enemy();
+            enemy.Damage(30);
+            Console.WriteLine(enemy.health);
+        }
+    }
+}
