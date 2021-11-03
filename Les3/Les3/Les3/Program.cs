18453261b63c43b5761ed4e6bc0ea0bacf643ba9
Jaap <jareerink@gmail.com>
2021-11-03 13:21:19 +0100

    les 3 af

---
+++ Les3/Les3/Les3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Les3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rechthoek = new Rectangle(100,100,50,60);
+            Rectangle vierkant = new Rectangle(50, 50, 50);
+            Rectangle vierkantBeweeg = new Rectangle(50);
+            Rectangle rechthoekBeweeg = new Rectangle(20, 30);
+
+            vierkantBeweeg.MoveTo(50, 50);
+
+            rechthoekBeweeg.Set(100, 100, 50, 60);
+        }
+    }
+}
---
+++ Les3/Les3/Les3/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les3
+{
+    class Rectangle
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        public Rectangle(int x,int y,int width,int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+
+        }
+        public Rectangle(int x, int y, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.x = size;
+            this.y = size;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public Rectangle(int size)
+        {
+            this.x = size;
+            this.y = size;
+        }
+
+        public void MoveTo(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public bool isSquare()
+        {
+            if(width == height)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Set(int x, int y,int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
