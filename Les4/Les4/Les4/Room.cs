c3e63ff278906b713d461608bbfa4ad536fd9337
Jaap <jareerink@gmail.com>
2021-11-04 16:06:20 +0100

    beginnetje aan les4

---
+++ Les4/Les4/Les4/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Les4
+{
+    class Room
+    {
+        int width;
+        int height;
+
+        int[][] tile = new int[10][];
+        public void Room(int width,int height)
+        {
+            this.width = width;
+            this.height = height;
+
+        }
+
+    }
+
+}
